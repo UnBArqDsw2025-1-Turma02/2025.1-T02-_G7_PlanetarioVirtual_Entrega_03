3aca1609c03f0023313e3836953be6574072cd0c
taybalau <taynaravitorino@gmail.com>
2025-06-02 23:39:37 -0300

    cors

--- projeto/grupo3/planetario-virtual-backend/src/main/java/com/ArqDSw/planetario_virtual_backend/configuration/OpenApiConfig.java
+++ projeto/grupo3/planetario-virtual-backend/src/main/java/com/ArqDSw/planetario_virtual_backend/configuration/OpenApiConfig.java
@@ -1,3 +1,5 @@
+package com.ArqDSw.planetario_virtual_backend.configuration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +11,11 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        Server productionServer = new Server().url("https://worthy-latia-planetario-virtual-02f6584c.koyeb.app");
-        Server localServer = new Server().url("http://localhost:8080");
+        Server productionServer = new Server();
+        productionServer.setUrl("https://worthy-latia-planetario-virtual-02f6584c.koyeb.app");
+
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
 
         return new OpenAPI()
                 .addServersItem(productionServer)
